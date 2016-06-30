be209dfc602076a5ac9ea88572492b2a3dcf6348
michellelle <upgrade2mode@gmail.com>
2016-06-30 17:41:31 +0000

    update action mailer

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMiler::Base.smtp_settings = {
+ActionMailer::Base.smtp_settings = {
     :address => 'stmp.sendgrid.net',
     :port => '587'
     :authentication => :plain,
