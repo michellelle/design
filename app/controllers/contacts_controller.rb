1894da5f7246206b5764921513a388453b9b2209
michellelle <upgrade2mode@gmail.com>
2016-06-30 15:28:22 +0000

    add validations and forms

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,9 +7,11 @@ class ContactsController < ApplicationController
         @contact = Contact.new(contact_params)
         
         if @contact.save
-            redirect_to new_contact_path, notice: "Message Sent."
+            flash[:success] = 'Thank you, message recieved.'
+            redirect_to new_contact_path
         else
-            redirect_to new_contact_path, notice: "Error Occured."
+            flash[:danger] = 'Error occured, message has not been sent.'
+            redirect_to new_contact_path
         end
     end
     
