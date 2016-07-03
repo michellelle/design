f71d575065264dc154d8487adf761046ce45ac49
michellelle <upgrade2mode@gmail.com>
2016-07-03 02:05:50 +0000

    Install Devise

--- config/environment.rb
+++ config/environment.rb
@@ -7,9 +7,15 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
     :address => 'stmp.sendgrid.net',
     :port => '587',
-    :authentication => :plain,
+}
+{
+    :authentication => :plain
+}
+{
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com'
+    :domain => 'heroku.com',
+}
+{
     :enable_startsls_auto => true
 }
