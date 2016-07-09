a229508cf1efad972eb9662941d693d4452ae920
michellelle <upgrade2mode@gmail.com>
2016-07-09 00:42:36 +0000

    fixed punctuation error in registration_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,9 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   protected
+  
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :srtipe_card_token, :email, :password, :password_confirmation) }
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :srtipe_card_token, :email, :password, 
+:password_confirmation) }
     end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,10 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+    
     def create
         super do |resource|
             if params[:plan]
-                resource.plan.id = params[:plan]
-                if resource.plan_id = 3
+                resource.plan_id = params[:plan]
+                if resource.plan_id == 3
                     resource.save_with_payment
                 else
                     resource.save
